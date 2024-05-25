99c6d8b491b285f1b824d82daf3d445c6251eb92
hakampong <hakamkam.01@gmail.com>
2024-05-25 15:49:44 +0800

    created taskcard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,4 +15,15 @@
             </div>
         </div>
     </div>
+
+    <div class="card" style="width: 18rem;">
+        <img src="public\images\beSide-logo.png" class="card-img-top" alt="">
+        <a href="#">
+            <div class="card-body">
+                <h5 class="card-title">Move furniture</h5>
+                <p class="card-text">I need help moving my sofa</p>
+                <p class="card-text"><small class="text-muted">Deadline: 05/05/2024</small></p>
+            </div>
+        </a>
+    </div>
 </x-app-layout>
