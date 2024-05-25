177a56ef410c6283cc11c8e5c5c06757e1ba2fca
Vignesh <bala12rupesh@gmail.com>
2024-05-26 05:11:27 +0800

    Updated login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,67 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+    <!-- Session Status -->
+    @if (session('status'))
+        <div class="alert alert-success mb-4">
+            {{ session('status') }}
         </div>
+    @endif
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+    <div class="container">
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <div class="mb-3">
+                <label for="name" class="form-label">{{ __('Name') }}</label>
+                <input id="name" class="form-control" type="name" name="name" value="{{ old('name') }}" required autofocus autocomplete="username">
+                @if ($errors->has('name'))
+                    <div class="text-danger mt-2">
+                        {{ $errors->first('name') }}
+                    </div>
+                @endif
+            </div>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+            <!-- Email Address -->
+            <div class="mb-3">
+                <label for="email" class="form-label">{{ __('Email') }}</label>
+                <input id="email" class="form-control" type="email" name="email" value="{{ old('email') }}" required autofocus autocomplete="username">
+                @if ($errors->has('email'))
+                    <div class="text-danger mt-2">
+                        {{ $errors->first('email') }}
+                    </div>
+                @endif
+            </div>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+            <!-- Password -->
+            <div class="mb-3">
+                <label for="password" class="form-label">{{ __('Password') }}</label>
+                <input id="password" class="form-control" type="password" name="password" required autocomplete="current-password">
+                @if ($errors->has('password'))
+                    <div class="text-danger mt-2">
+                        {{ $errors->first('password') }}
+                    </div>
+                @endif
+            </div>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <!-- Confirm Password -->
+            <div class="mb-3">
+                <label for="password_confirmation" class="form-label">{{ __('Confirm Password') }}</label>
+                <input id="password_confirmation" class="form-control" type="password" name="password_confirmation" required autocomplete="new-password">
+                @if ($errors->has('password_confirmation'))
+                    <div class="text-danger mt-2">
+                        {{ $errors->first('password_confirmation') }}
+                    </div>
+                @endif
+            </div>
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+            <div class="d-flex justify-content-end mt-3">
+                <a class="btn btn-outline-success me-2" href="{{ route('login') }}">
+                    {{ __('Back to Login') }}
+                </a>
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
+                <button type="submit" class="btn btn-success">
+                    {{ __('Register') }}
+                </button>
+            </div>
+        </form>
+    </div>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,19 +19,22 @@
         <script src="/bootstrap/masonry.pkgd.min.js"></script>
         <script src="/bootstrap/bootstrap.bundle.js"></script>
         <link href="/css/app.css" rel="stylesheet" />
-        <script src="/js/app.js"></script>    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <div class="center-logo">
-                        <img src="{{ asset('images/beSide-logo.png') }}" alt="Logo" class="logo">
-                    </div>
-                </a>
-            </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
+        <script src="/js/app.js"></script>    
+    </head>
+    <body>
+        <div class="d-flex align-items-center justify-content-center" style="height: 100vh;">
+            <div class="align-items-center p-5 shadow-box">
+                <div>
+                    <a href="/">
+                        <div class="center-logo mb-3">
+                            <img src="{{ asset('images/beSide-logo.png') }}" alt="Logo" width="250">
+                        </div>
+                    </a>
+                </div>
+    
+                <div>
+                    {{ $slot }}
+                </div>
             </div>
         </div>
     </body>
