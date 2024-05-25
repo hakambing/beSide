af64542fac228130547fcf7b52d26de65eaf98a3
Vignesh <bala12rupesh@gmail.com>
2024-05-26 04:26:22 +0800

    Updated logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,47 +6,53 @@
         </div>
     @endif
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+    <div class="container">
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
 
-        <!-- Email Address -->
-        <div class="mb-3">
-            <label for="email" class="form-label">{{ __('Email') }}</label>
-            <input id="email" class="form-control" type="email" name="email" value="{{ old('email') }}" required autofocus autocomplete="username">
-            @if ($errors->has('email'))
-                <div class="text-danger mt-2">
-                    {{ $errors->first('email') }}
-                </div>
-            @endif
-        </div>
+            <!-- Email Address -->
+            <div class="mb-3">
+                <label for="email" class="form-label">{{ __('Email') }}</label>
+                <input id="email" class="form-control" type="email" name="email" value="{{ old('email') }}" required autofocus autocomplete="username">
+                @if ($errors->has('email'))
+                    <div class="text-danger mt-2">
+                        {{ $errors->first('email') }}
+                    </div>
+                @endif
+            </div>
 
-        <!-- Password -->
-        <div class="mb-3">
-            <label for="password" class="form-label">{{ __('Password') }}</label>
-            <input id="password" class="form-control" type="password" name="password" required autocomplete="current-password">
-            @if ($errors->has('password'))
-                <div class="text-danger mt-2">
-                    {{ $errors->first('password') }}
-                </div>
-            @endif
-        </div>
+            <!-- Password -->
+            <div class="mb-3">
+                <label for="password" class="form-label">{{ __('Password') }}</label>
+                <input id="password" class="form-control" type="password" name="password" required autocomplete="current-password">
+                @if ($errors->has('password'))
+                    <div class="text-danger mt-2">
+                        {{ $errors->first('password') }}
+                    </div>
+                @endif
+            </div>
 
-        <!-- Remember Me -->
-        <div class="form-check mb-3">
-            <input id="remember_me" type="checkbox" class="form-check-input" name="remember">
-            <label for="remember_me" class="form-check-label">{{ __('Remember me') }}</label>
-        </div>
+            <!-- Remember Me -->
+            <div class="form-check mb-3">
+                <input id="remember_me" type="checkbox" class="form-check-input" name="remember">
+                <label for="remember_me" class="form-check-label">{{ __('Remember me') }}</label>
+            </div>
 
-        <div class="d-flex justify-content-end">
-            @if (Route::has('password.request'))
-                <a class="text-sm text-decoration-none me-3" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+            <div class="d-flex justify-content-end">
+                @if (Route::has('password.request'))
+                    <a class="text-sm text-decoration-none me-3" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                @endif
+
+                <a class="btn btn-outline-success me-2" href="{{ route('register') }}">
+                    {{ __('Register') }}
                 </a>
-            @endif
 
-            <button type="submit" class="btn btn-primary">
-                {{ __('Log in') }}
-            </button>
-        </div>
-    </form>
+                <button type="submit" class="btn btn-success">
+                    {{ __('Log in') }}
+                </button>
+            </div>
+        </form>
+    </div>
 </x-guest-layout>
