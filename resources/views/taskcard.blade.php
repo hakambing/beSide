e08ec9cd70f8d594e2a3341821f29b803dacd014
Vignesh <bala12rupesh@gmail.com>
2024-05-26 08:57:03 +0800

    Updated task front page

--- resources/views/taskcard.blade.php
+++ resources/views/taskcard.blade.php
@@ -1,20 +1,28 @@
 <x-app-layout>
-    <x-slot name="header">
-        <div class="container text-center">
-            <div class="row">
-                <div class="col align-self-start">
-
-                    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                        {{ __('Tasks') }}
-                    </h2>
-                </div>
-                <div class="col align-self-end">
-                    <a href="/dashboard/create-task" class="btn btn-primary">Request for
-                        Help</a>
-                </div>
-    </x-slot>
-
     <div class="container">
+        <div class="mt-4 row">
+            <div class="col-6 align-self-start">
+                <h1 class="">
+                    Tasks
+                </h1>
+            </div>
+            <div class="col-6">
+                <a href="/dashboard/create-task" class="btn btn-primary btn-lg align-self-end float-md-end">Request for
+                    Help</a>
+            </div>
+            <div class="col-6 align-self-start">
+                <span class="fs-3 text-muted lh-sm">
+                    @if (count($tasks) == 0)
+                        There are <b class="color-primary">no</b> neighbours near your block that need your help. You can get the ball rolling!
+                    @elseif (count($tasks) == 1)
+                        There is <b class="color-primary">1</b> neighbour near your block that need your help.
+                    @else
+                        There are <b class="color-primary">{{count($tasks)}}</b> neighbours near your block that need your help.
+                    @endif
+                </span>
+            </div>
+        </div>
+
         <div class="row gx-4 gy-4"> <!-- Bootstrap classes to add horizontal and vertical gaps -->
             @foreach ($tasks as $task)
                 <div class="col-md-4 col-sm-6 mb-4"> <!-- Bootstrap column with margin-bottom -->
