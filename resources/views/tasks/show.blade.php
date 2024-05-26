23780b6e8ac65eab0e6b0da6107d29b90be50ca1
Vignesh <bala12rupesh@gmail.com>
2024-05-26 09:39:50 +0800

    Update show.blade.php

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,24 +1,19 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Task Details') }}
-        </h2>
-    </x-slot>
-
-
-
-    <div class="row g-0 text-center">
-        <div class="col-sm-6 col-md-8">
-            <div class="card-body">
-                <h2 class="h2">{{ $task->title }}</h2>
-                <p class="card-text">{{ $task->description }}</p>
-                <p class="card-text"><small class="text-muted">Deadline:
-                        {{ optional($task->deadline)->format('Y-m-d') }}</small></p>
+    <div class="container">
+        <div class="mt-4 row">
+            <div class="col-12 align-self-start">
+                <h1 class="">
+                    {{ $task->title }}
+                </h1>
             </div>
-        </div>
-        <div class="col-6 col-md-4">
-
+            <div class="col-12 align-self-start">
+                <span class="fs-3 text-muted lh-sm">
+                    {{ $task->description }}
+                    {{ dd($task) }}
+                </span>
+            </div>
+            <p class="card-text mt-2"><small class="text-muted">Deadline:
+                {{ optional($task->deadline)->format('Y-m-d') }}</small></p>
         </div>
     </div>
-
 </x-app-layout>
